57e3d4050a09471b65059994ffa618421f6fe2a0
Matthew Coburn <coburn@dandere.net>
2018-12-22 13:43:25 +1000

    EXPERIMENTAL 2018 BRANCH - Update to NET 4.6 - DOES NOT CURRENTLY WORK

--- IgnoranceTransport/IgnoranceTransport.cs
+++ IgnoranceTransport/IgnoranceTransport.cs
@@ -14,7 +14,9 @@
 // It would be greatly appreciated if you reported bugs
 // and donate coffee at https://github.com/SoftwareGuy/Ignorance.
 // ----------------------------------------
-
+// THIS IS THE MIRROR 2018 BRANCH OF IGNORANCE.
+// DO NOT USE IF YOU ARE USING UNITY 2017 LTS.
+// ----------------------------------------
 using ENet;
 using System;
 using System.Collections.Generic;
@@ -29,6 +31,10 @@ namespace Mirror
     /// </summary>
     public class IgnoranceTransport : TransportLayer
     {
+        // -- GENERAL VARIABLES -- //
+        private const string TransportVersion = "2018-1.1.0";
+        private bool libraryInitialized = false;
+
         // -- EXPOSED PUBLIC VARIABLES -- //
         public bool verboseLoggingEnabled = false;
 
@@ -37,19 +43,15 @@ namespace Mirror
         public uint peerBaseTimeout = 5000;        // 5000 ticks (5 seconds)
         public uint peerBaseTimeoutMultiplier = 3; // peerBaseTimemout * this value = maximum time waiting until client is removed
 
-        // -- GENERAL VARIABLES -- //
-        private const string TransportVersion = "v1.0.9.1";
-        private bool libraryInitialized = false;
         // -- SERVER WORLD VARIABLES -- //
         private Host server;
+        private Host client;
+
         private Address serverAddress;
+        private Peer clientPeer;
 
         private Dictionary<int, Peer> knownPeerConnections;   // Known connections dictonary since ENET is a little weird.
         private int serverConnectionCount = 1;                // Used by our dictionary to map ENET Peers to connections. Start at 1 just to be safe.
-
-        // -- CLIENT WORLD VARIABLES -- //
-        private Host client;
-        private Peer clientPeer;
 
         // This section defines what classic UNET channels refer to.
         private PacketFlags[] packetSendMethods =
@@ -58,32 +60,127 @@ namespace Mirror
             PacketFlags.None       // Channels.DefaultUnreliable
         };
 
+        // -- IGNORANCE 2018 ASYNC ACTIONS -- //
+        public event Action OnClientConnect;
+        public event Action<byte[]> OnClientData;
+        public event Action<Exception> OnClientError;
+        public event Action OnClientDisconnect;
+        public event Action<int> OnServerConnect;
+        public event Action<int, byte[]> OnServerData;
+        public event Action<int, Exception> OnServerError;
+        public event Action<int> OnServerDisconnect;
+
         // -- INITIALIZATION -- // 
         public IgnoranceTransport()
         {
-            Debug.LogFormat("Ignorance Transport {0} ready! Report bugs and donate coffee at https://github.com/SoftwareGuy/Ignorance.", TransportVersion);
+            Debug.LogFormat("EXPERIMENTAL Ignorance build {0} for Mirror 2018 ready! Report bugs and donate coffee at https://github.com/SoftwareGuy/Ignorance.", TransportVersion);
         }
 
-        /// <summary>
-        /// Gets the maximum packet size allowed. Introduced from Mirror upstream git commit: 1289dee8. <para />
-        /// Please see https://github.com/nxrighthere/ENet-CSharp/issues/33 for more information.
-        /// </summary>
-        /// <returns>A integer with the maximum packet size.</returns>
-        public int GetMaxPacketSize(int channelId)
-        {
-            return (int)Library.maxPacketSize;  // 33,554,432 bytes. Do not attempt to send more, ENET will likely catch fire. NX's orders.
+        // -- CLIENT WORLD -- //
+        public virtual bool ClientConnected() {
+            return clientPeer.IsSet && clientPeer.State == PeerState.Connected;
         }
 
-        // -- SERVER WORLD FUNCTIONS -- //
-        /// <summary>
-        /// Gets info about a connection via the connectionId. <para />
-        /// Apparently it only gets the IP Address. What's up with that?
-        /// </summary>
-        /// <param name="connectionId">The connection ID to lookup.</param>
-        /// <param name="address">The IP Address. This is what will be returned, don't fill this in!</param>
-        /// <returns>The IP Address of the connection. Returns (invalid) if it cannot find it in the dictionary.</returns>
-        public bool GetConnectionInfo(int connectionId, out string address)
+        public virtual void ClientConnect(string address, int port) {
+            // Fire up ENET-C#'s backend.
+            if (!libraryInitialized)
+            {
+                Library.Initialize();
+                libraryInitialized = true;
+            }
+
+            // Setup our references.
+            client = new Host();
+            Address clientAddress = new Address();
+
+            clientAddress.SetHost(address);
+            clientAddress.Port = (ushort)port;
+
+            // Create the client.
+            client.Create();
+
+            // Connect the client to the server.
+            clientPeer = client.Connect(clientAddress);
+        }
+
+        public virtual void ClientSend(int channelId, byte[] data) {
+            // STUBBED FOR NOW
+            // Need to talk to Mirror devs for assistance.
+        }
+
+        
+
+        public virtual void ClientDisconnect() {
+            if (clientPeer.IsSet) clientPeer.DisconnectNow(0);
+
+            if (client != null)
+            {
+                // client.Flush();
+                client.Dispose();
+            }
+
+            client = null;
+        }
+
+        // -- SERVER WORLD -- //
+        public virtual bool ServerActive() {
+            if (!libraryInitialized) return false;
+
+            if (server != null) return server.IsSet;
+            else return false;
+        }
+
+        public virtual void ServerStart(string address, int port, int maxConnections)
         {
+            Debug.LogFormat("Ignorance Transport: Starting up server. {0} port {1} with {2} connection capacity.", address ?? "(null)", port, maxConnections);
+            // Fire up ENET-C#'s backend.
+            if (!libraryInitialized)
+            {
+                Library.Initialize();
+                libraryInitialized = true;
+            }
+
+            // Initialize our references.
+            server = new Host();
+            serverAddress = new Address();
+            knownPeerConnections = new Dictionary<int, Peer>();
+
+            // Bind if we have an address specified.
+            if (!string.IsNullOrEmpty(address))
+            {
+                Debug.LogFormat("Ignorance Transport: Binding to {0}", address);
+                serverAddress.SetHost(address);
+            }
+
+            serverAddress.Port = (ushort)port;
+
+            // Finally create the server.
+            server.Create(serverAddress, maxConnections);
+
+            Debug.Log("Ignorance Transport: Server successfully started if you see this message.");
+        }
+
+        public virtual void ServerStartWebsockets(string address, int port, int maxConnections)
+        {
+            Debug.LogError("Ignorance Transport does not support Websockets. Aborting...");
+        }
+
+        public virtual void ServerSend(int connectionId, int channelId, byte[] data) {
+            // STUBBED FOR NOW
+            // Need to talk to Mirror devs for assistance.
+        }
+
+        public virtual bool ServerDisconnect(int connectionId)
+        {
+            if (knownPeerConnections.ContainsKey(connectionId)) {
+                knownPeerConnections[connectionId].Disconnect(0);
+                return true;
+            }
+
+            return false;
+        }
+
+        public virtual bool GetConnectionInfo(int connectionId, out string address) {
             address = "(invalid)";
 
             if (knownPeerConnections.ContainsKey(connectionId))
@@ -95,32 +192,57 @@ namespace Mirror
             return false;
         }
 
-        /// <summary>
-        /// Is the server active?
-        /// </summary>
-        /// <returns>True if the server is active, false otherwise.</returns>
-        public bool ServerActive()
-        {
-            if (!libraryInitialized) return false;
-
-            if (server != null) return server.IsSet;
-            else return false;
-        }
-
-        /// <summary>
-        /// Disconnects a server connection.
-        /// </summary>
-        /// <param name="connectionId">The connection ID to evict.</param>
-        /// <returns>True if the connection exists, false otherwise.</returns>
-        public bool ServerDisconnect(int connectionId)
-        {
-            if (knownPeerConnections.ContainsKey(connectionId))
+        public virtual void ServerStop() {
+            foreach (KeyValuePair<int, Peer> entry in knownPeerConnections)
             {
-                knownPeerConnections[connectionId].Disconnect(0);
-                return true;
+                entry.Value.DisconnectNow(0);
             }
 
-            return false;
+            // Don't forget to dispose stuff.
+            if (server != null)
+            {
+                server.Flush(); // testing
+                server.Dispose();
+            }
+            server = null;
+        }
+
+        public virtual void Shutdown()
+        {
+            Debug.Log("Ignorance Transport: Going down for shutdown NOW!");
+
+            // Shutdown the client first.
+            if (client != null && client.IsSet)
+            {
+                if (verboseLoggingEnabled) Debug.Log("Sending the client process to the dumpster fire...");
+
+                if (clientPeer.IsSet) clientPeer.DisconnectNow(0);
+
+                client.Flush();
+                client.Dispose();
+            }
+
+            // Shutdown the server.
+            if (server != null && server.IsSet)
+            {
+                if (verboseLoggingEnabled) Debug.Log("Sending the server process to the dumpster fire...");
+
+                server.Flush();
+                server.Dispose();
+            }
+
+            Library.Deinitialize();
+            libraryInitialized = false;
+            Debug.Log("Ignorance Transport shutdown complete. Have a good one.");
+        }
+        /// <summary>
+        /// Gets the maximum packet size allowed. Introduced from Mirror upstream git commit: 1289dee8. <para />
+        /// Please see https://github.com/nxrighthere/ENet-CSharp/issues/33 for more information.
+        /// </summary>
+        /// <returns>A integer with the maximum packet size.</returns>
+        public int GetMaxPacketSize(int channelId)
+        {
+            return (int)Library.maxPacketSize;  // 33,554,432 bytes. Do not attempt to send more, ENET will likely catch fire. NX's orders.
         }
 
         /// <summary>
@@ -130,6 +252,7 @@ namespace Mirror
         /// <param name="transportEvent">What event is this? Connection? Data? Disconnection?</param>
         /// <param name="data">Byte array of the data payload being received.</param>
         /// <returns>True if successful, False if unsuccessful.</returns>
+        /*
         public bool ServerGetNextMessage(out int connectionId, out TransportEvent transportEvent, out byte[] data)
         {
             // The incoming Enet Event.
@@ -232,7 +355,7 @@ namespace Mirror
             // We're done here. Bugger off.
             return true;
         }
-
+        */
         /// <summary>
         /// Send data on the server side.
         /// </summary>
@@ -240,6 +363,7 @@ namespace Mirror
         /// <param name="channelId">The channel ID to send data on. Must not be lower or greater than the values in the sendMethods array.</param>
         /// <param name="data">The payload to transmit.</param>
         /// <returns></returns>
+        /*
         public bool ServerSend(int connectionId, int channelId, byte[] data)
         {
             // Another mailing pigeon
@@ -271,144 +395,7 @@ namespace Mirror
                 if (verboseLoggingEnabled) Debug.LogFormat("Ignorance Transport: ServerSend() to fakeConnID {0} on channel {1} failure", connectionId, channelId);
                 return false;
             }
-        }
-
-        /// <summary>
-        /// Start the server with the specified parameters.
-        /// </summary>
-        /// <param name="address">The address to bind to.</param>
-        /// <param name="port">The port to use. Do not run more than one server on the same port.</param>
-        /// <param name="maxConnections">How many connections can we have?</param>
-        public void ServerStart(string address, int port, int maxConnections)
-        {
-            // Fire up ENET-C#'s backend.
-            if (!libraryInitialized)
-            {
-                Library.Initialize();
-                libraryInitialized = true;
-            }
-
-            server = new Host();
-            serverAddress = new Address();
-            knownPeerConnections = new Dictionary<int, Peer>();
-
-            if (verboseLoggingEnabled) Debug.LogFormat("Ignorance Transport: ServerStart(): {0}, {1}, {2}", address ?? "(null)", port, maxConnections);
-            // Version 1.0.4: undo what 1.0.2/1.0.3 did regarding this.
-            if (!string.IsNullOrEmpty(address))
-            {
-                Debug.LogFormat("Ignorance Transport: Binding to address {0}", address);
-                serverAddress.SetHost(address);
-            }
-
-            // Setup the port.
-            serverAddress.Port = (ushort)port;
-
-            // Finally create the server.
-            server.Create(serverAddress, maxConnections);
-            
-            // Log our best effort attempts
-            Debug.LogFormat("Ignorance Transport: Attempted to create server with capacity of {0} connections on UDP port {1}", maxConnections, Convert.ToUInt16(port));
-            Debug.LogFormat("Ignorance Transport: If you see this, the server most likely was successfully created and started! (This is good.)");
-        }
-
-        /// <summary>
-        /// Start the websockets version of the server.
-        /// NOT IMPLEMENTED AND PROBABLY NEVER WILL BE. DO NOT USE!
-        /// </summary>
-        /// <param name="address">The address to bind to.</param>
-        /// <param name="port">The port to use. Do not run more than one server on the same port.</param>
-        /// <param name="maxConnections">How many connections can we have?</param>
-        public void ServerStartWebsockets(string address, int port, int maxConnections)
-        {
-            // Websockets? Nani?
-            throw new NotImplementedException("WebSockets with ENET are not possible and probably will never be implemented. Sorry to disappoint");
-        }
-
-        /// <summary>
-        /// Called when the server stops.
-        /// </summary>
-        public void ServerStop()
-        {
-            if (verboseLoggingEnabled) Debug.LogFormat("Ignorance Transport: ServerStop()");
-
-            foreach (KeyValuePair<int, Peer> entry in knownPeerConnections)
-            {
-                entry.Value.DisconnectNow(0);
-            }
-
-            // Don't forget to dispose stuff.
-            if (server != null)
-            {
-                server.Flush(); // testing
-                server.Dispose();
-            }
-            server = null;
-        }
-
-        // -- CLIENT WORLD FUNCTIONS -- //
-        /// <summary>
-        /// Connects the client to a server.
-        /// </summary>
-        /// <param name="address">The connection address.</param>
-        /// <param name="port">The connection port.</param>
-        public void ClientConnect(string address, int port)
-        {
-            // Fire up ENET-C#'s backend.
-            if (!libraryInitialized)
-            {
-                Library.Initialize();
-                libraryInitialized = true;
-            }
-
-            if (verboseLoggingEnabled) Debug.LogFormat("Ignorance Transport: ClientConnect({0}, {1})", address, port);
-            client = new Host();          
-            Address clientAddress = new Address();
-
-            // Set hostname and port to connect to.
-            clientAddress.SetHost(address);
-            clientAddress.Port = (ushort)port; // Convert.ToUInt16(port);
-
-            // Create the client.
-            client.Create();
-
-            // Connect the client to the server.
-            clientPeer = client.Connect(clientAddress);
-
-            // Debugging only
-            if (verboseLoggingEnabled) Debug.LogWarning("Ignorance Transport: clientPeer isSet? " + clientPeer.IsSet);
-        }
-
-        /// <summary>
-        /// Is the client connected currently?
-        /// </summary>
-        /// <returns>True if connected, False if not.</returns>
-        public bool ClientConnected()
-        {
-            if (verboseLoggingEnabled) Debug.Log("Ignorance Transport: ClientConnected() called");
-            return clientPeer.IsSet && clientPeer.State == PeerState.Connected;
-        }
-
-        /// <summary>
-        /// Disconnect the client.
-        /// </summary>
-        public void ClientDisconnect()
-        {
-            if (verboseLoggingEnabled) Debug.Log("Ignorance Transport: ClientDisconnect()");
-
-            // TODO: I dunno what to put here! nx has something about Reasons for the disconnection??
-            if (clientPeer.IsSet)
-            {
-                clientPeer.DisconnectNow(0);
-            }
-
-            if(client != null)
-            {
-                client.Flush(); // testing
-                client.Dispose();
-            }
-
-            client = null;
-        }
+        } */
 
         /// <summary>
         /// Get the next client data packet.
@@ -484,6 +471,7 @@ namespace Mirror
         /// <param name="channelId">The channel ID.</param>
         /// <param name="data">The data array.</param>
         /// <returns></returns>
+        /*
         public bool ClientSend(int channelId, byte[] data)
         {
             Packet mailingPigeon = default(Packet);
@@ -514,41 +502,9 @@ namespace Mirror
 
             return sendingPacketWasSuccessful;
         }
-
+        */
         // -- END CLIENT FUNCTIONS -- //
 
-        // -- SHUTDOWN FUNCTIONS -- //
-        /// <summary>
-        /// Shuts down the transport.
-        /// </summary>
-        public void Shutdown()
-        {
-            Debug.Log("Ignorance Transport: Going down for shutdown NOW!");
-
-            // Shutdown the client first.
-            if (client != null && client.IsSet)
-            {
-                if (verboseLoggingEnabled) Debug.Log("Sending the client process to the dumpster fire...");
-
-                if (clientPeer.IsSet) clientPeer.DisconnectNow(0);
-
-                client.Flush();
-                client.Dispose();
-            }
-
-            // Shutdown the server.
-            if (server != null && server.IsSet)
-            {
-                if (verboseLoggingEnabled) Debug.Log("Sending the server process to the dumpster fire...");
-
-                server.Flush();
-                server.Dispose();
-            }
-
-            Library.Deinitialize();
-            libraryInitialized = false;
-            Debug.Log("Ignorance Transport shutdown complete. Have a good one.");
-        }
 
         // -- TIMEOUT SETTINGS -- //
         /// <summary>
@@ -719,6 +675,16 @@ namespace Mirror
         public void EnableParanoidLogging(bool enable)
         {
             verboseLoggingEnabled = enable;
+        }
+
+        void TransportLayer.ClientSend(int channelId, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        void TransportLayer.ServerSend(int connectionId, int channelId, byte[] data)
+        {
+            throw new NotImplementedException();
         }
     }
 }
