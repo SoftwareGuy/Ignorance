0851048b991d856ede3222aaf6280a045622a4d7
petris <paprikapolishgamer@gmail.com>
2019-02-20 19:57:07 +0100

    Make a IDissonanceTransport interface

---
+++ Assets/Mirror/Transports/Ignorance/IDissonanceTransport.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public interface IDissonanceTransport
+{
+    bool ServerSend(int connectionId, int channelId, ArraySegment<byte> data);
+    bool ClientSend(int channelId, ArraySegment<byte> data);
+}
