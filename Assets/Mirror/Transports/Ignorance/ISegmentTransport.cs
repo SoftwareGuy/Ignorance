d8aff90af163f117bf9acccd032171b45cb27497
Matt Coburn <coburn@dandere.net>
2019-02-21 11:47:10 +1000

    owo whats this

--- Assets/Mirror/Transports/Ignorance/ISegmentTransport.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-public interface ISegmentTransport
-{
-    bool ServerSend(int connectionId, int channelId, ArraySegment<byte> data);
-    bool ClientSend(int channelId, ArraySegment<byte> data);
-}
