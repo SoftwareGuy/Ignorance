b7e6418ab96eae7b36b8d75defd0a1b3025d0278
Matt Coburn <coburn@dandere.net>
2020-08-21 19:22:26 +1000

    Update native library debugger

--- Assets/Mirror/Runtime/Transport/Ignorance/Editor/NativeLibraryDebugger.cs
+++ Assets/Mirror/Runtime/Transport/Ignorance/Editor/NativeLibraryDebugger.cs
@@ -9,7 +9,7 @@ namespace Mirror
         [MenuItem("Ignorance Debugging/Enet Library Name")]
         public static void RevealEnetLibraryName()
         {
-            EditorUtility.DisplayDialog("Enet Library Name", ENet.Native.nativeLibraryName, "Got it");
+            EditorUtility.DisplayDialog("Enet Library Name", $"Use this for debugging.\nYour platform expects the native Enet library to be called: {ENet.Native.nativeLibraryName}", "Got it");
         }
     }
 }
