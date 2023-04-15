1939de9733e8e42f04fee480b463c1311e33cd31
Matt Coburn <coburn@dandere.net>
2023-04-15 14:06:09 +1000

    version bump

--- Assets/Mirror/Transports/Ignorance/IgnoranceDefinitions.cs
+++ Assets/Mirror/Transports/Ignorance/IgnoranceDefinitions.cs
@@ -25,7 +25,7 @@ namespace IgnoranceCore
 
     public class IgnoranceInternals
     {
-        public const string Version = "1.4.0r2 (LTS)";
+        public const string Version = "1.4.1r0 (LTS)";
         public const string Scheme = "enet";
         public const string BindAnyAddress = "::0";
     }
